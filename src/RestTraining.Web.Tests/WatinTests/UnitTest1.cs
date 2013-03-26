01574fd3ec281ee92e3f02aeb27254f13661b4a5
Bogdan Dudnik <Bogdan.Dudnik@dataart.com>
2013-03-26 18:42:25 +0200

    added datrange validation and added watin to project to create tests

---
+++ src/RestTraining.Web.Tests/WatinTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WatiN.Core;
+
+namespace RestTraining.Web.Tests.WatinTests
+{
+    [TestClass]
+    public class HotelTests
+    {
+        [TestMethod]
+        public void AddBoundedHotelTest()
+        {
+            using (var browser = new IE())
+            {
+                browser.GoTo("http://localhost:51599/");
+                var link = browser.Link(Find.ByText("Hotels"));
+                link.Click();
+            }
+        }
+    }
+}
