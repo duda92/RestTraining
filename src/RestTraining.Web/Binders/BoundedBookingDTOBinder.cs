7751fa716c8c068c37d78d439639998472afacaf
Bogdan Dudnik <Bogdan.Dudnik@dataart.com>
2013-04-04 12:50:31 +0300

    added jasny - Bootstrap extensions, improved ui for web forms

---
+++ src/RestTraining.Web/Binders/BoundedBookingDTOBinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace RestTraining.Web.Binders
+{
+    public class BoundedBookingDTOBinder : DefaultModelBinder
+    {
+        protected override void BindProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, System.ComponentModel.PropertyDescriptor propertyDescriptor)
+        {
+            if (propertyDescriptor.Name == "BoundedPeriodId")
+            {
+                string periodIdString = bindingContext.ValueProvider.GetValue("BoundedPeriodId").AttemptedValue;
+                int periodId = 0;
+                int.TryParse(periodIdString, out periodId);
+                propertyDescriptor.SetValue(bindingContext.Model, periodId);
+                return;
+            }
+            base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
+        }
+    }
+}
