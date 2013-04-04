29473a7bac9e1dde721dccd3e273536db99bf6d8
Bogdan Dudnik <Bogdan.Dudnik@dataart.com>
2013-04-04 14:29:51 +0300

    fixed validation error on edit hotelnumber page

--- src/RestTraining/DTO/IncludedItemDTO.cs
+++ src/RestTraining/DTO/IncludedItemDTO.cs
@@ -1,4 +1,5 @@
 ﻿using RestTraining.Api.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace RestTraining.Api.DTO
 {
@@ -6,6 +7,7 @@ namespace RestTraining.Api.DTO
     {
         public IncludeItemType IncludeItemType { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Invalid count of included item")]
         public int Count { get; set; }
     }
 }
